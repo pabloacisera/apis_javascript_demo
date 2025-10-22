86a49e99e00272d0c15d6b3730e4c31e4bdcfcb0
pabloacisera <pabloacisera2021@gmail.com>
2025-10-22 18:38:34 -0300

    updated notification api with interface

--- index.php
+++ index.php
@@ -60,13 +60,28 @@
             /* Algunos navegadores lo respetan */
             color: #000;
         }
+
+        .logo {
+            margin: auto;
+            background-color: #000;
+            text-align: center;
+            margin-bottom: 2rem
+        }
     </style>
+
+    <div class="logo">
+        <svg xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 -960 960 960" width="64px" fill="#FFFF55">
+            <path
+                d="M274-360q-15 0-24.5-9.5T240-394v-66h48v52h84v-192h48v206q0 15-9.5 24.5T386-360H274Zm240 0q-15 0-24.5-9.5T480-394v-46h48v32h104v-53H514q-14 0-24-10t-10-24v-71q0-15 9.5-24.5T514-600h132q15 0 24.5 9.5T680-566v46h-48v-32H528v53h118q14 0 24 10t10 24v71q0 15-9.5 24.5T646-360H514Z" />
+        </svg>
+    </div>
 
     <h3 class="title">Listado de apis</h3>
 
     <select name="api_list" id="apiList">
-        <option value=""></option>
-        <option value="broadcast">api broadcast</option>
+        <option>seleccionar api</option>
+        <option value="broadcast">API Broadcast</option>
+        <option value="notification">API Notification</option>
     </select>
 
     <div id="container"></div>
--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,13 @@
 let container = document.getElementById("container");
 
 async function renderPage(path) {
-
     let filePath = `templates/${path}`;
-    let file = await fetch(filePath);
-
-    let result = await file.text();
-
-    container.innerHTML = result;
-
-    // 🔧 Ejecutar scripts del HTML inyectado
-    const scripts = container.querySelectorAll("script");
-    scripts.forEach(oldScript => {
-        const newScript = document.createElement("script");
-        if (oldScript.src) {
-            // si tiene atributo src, lo cargamos externamente
-            newScript.src = oldScript.src;
-        } else {
-            // si tiene código inline, lo copiamos
-            newScript.textContent = oldScript.textContent;
-        }
-        document.body.appendChild(newScript);
-        oldScript.remove(); // limpieza opcional
-    });
-
+    window.location.href = filePath;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     let listItem = document.querySelector("#apiList").addEventListener("change", (function (event) {
-
         let newPath = (`${event.target.value}.html`);
-
         renderPage(newPath);
     }));
 });
